f5eb74ab854f0a71b43ceb76e1d057308253c8ab
Rodrigo Garcia <rodrigogarciahoyos@gmail.com>
2017-12-13 16:17:22 -0500

    casi

--- js/app.js
+++ js/app.js
@@ -7,16 +7,14 @@ $(function() {
 	$('#entrar').click(function(event) {
 		var key=$('#key').val();
 		var uri=url+'key/'+key;
-		console.log(uri);
 		$.getJSON(uri, function(json, textStatus) {
 			if(json.length==0){
 				alert('Pin incorrecto');
 			}else{
-				console.log(json[1]);
-				db.transaction(function (tx) {
-				  tx.executeSql('INSERT INTO cobro (cobro_id,key) VALUES ('+json[1].cobro_id+','+json[1].key+')');
+				$.each( json, function( key, value ) {
+					location.href ='user.html?cobro='+value.cobro_id+'&key='+value.key+'';
 				});
-				location.href ="app.html";
+				
 			}
 		});
 	});
@@ -49,13 +47,22 @@ function sync(){
 		            var cobro=(results.rows.item(i).cobro_id);
 		            var keys=results.rows.item(i).key;
 		            	var uri=url+'prestamos/'+cobro;
-						console.log(uri);
 						$.getJSON(uri, function(json, textStatus) {
+							
+							var x=0;
 							$.each( json, function( key, val ) {
-							   console.log(val);
+
 							   	db.transaction(function (tx) {
 								  tx.executeSql('INSERT INTO prestamos (prestamo,nombre,identificacion,cuota,key) VALUES ("'+val.ide+'","'+val.nombre+'","'+val.identificacion+'","'+val.cuota+'","'+keys+'")');
+								  x=x+1;
+									if(json.length==x){
+								   		console.log(json.length+' '+x);
+								   		location.href ="app.html";
+								   	}
 								});
+							   	
+
+
 							});
 						});
 		          }
@@ -65,7 +72,7 @@ function sync(){
 
 		}).fail( function() {
 
-		    alert( 'Error intenete mas tarde!!' );
+		    alert( 'Error conexion intenete mas tarde!!' );
 
 		});	
 
@@ -94,30 +101,41 @@ function mostrar_prestamos(){
 '<th>Cuota</th>'+
 '<th>Accion</th>'+
 '</tr>';
+		var user=null;
       db.transaction(function (tx) {
-        tx.executeSql('SELECT * FROM prestamos', [], function (tx, results) {
+        tx.executeSql('SELECT * FROM cobro', [], function (tx, results) {
+          var len = results.rows.length, i;
+          for (i = 0; i < len; i++) {
+          	user=results.rows.item(i).user;
+          }
+
+        console.log(user);
+                tx.executeSql('SELECT * FROM prestamos', [], function (tx, results) {
           var len = results.rows.length, i;
           for (i = 0; i < len; i++) {
           	 d+= '<tr id="'+results.rows.item(i).prestamo+'">'+
 					 '<td>'+results.rows.item(i).nombre+'</td>'+
 					 '<td>'+results.rows.item(i).identificacion+'</td>'+
 					 '<td>'+results.rows.item(i).cuota+'</td>'+
-					 '<td style="text-align: center;background: #512da8;color: #fff;font-weight: bold;" id="btn'+results.rows.item(i).prestamo+'" onclick="pagar('+results.rows.item(i).cuota+','+results.rows.item(i).prestamo+','+results.rows.item(i).key+')" >Pagar</td>'+
+					 '<td style="text-align: center;background: #512da8;color: #fff;font-weight: bold;" id="btn'+results.rows.item(i).prestamo+'" onclick="pagar('+results.rows.item(i).cuota+','+results.rows.item(i).prestamo+','+results.rows.item(i).key+','+user+')" >Pagar</td>'+
 					 '</tr>';
           }
            $("#tabla").append(d);
         });
 
-      });	
+        });
+
+      });
 }
 
-function pagar(cuota,prestamo,key){
+function pagar(cuota,prestamo,key,user){
+	console.log(user);
 	var x = prompt("Digite Monto a pagar", cuota);
 	var r = confirm("Estas seguro de pagar?");
 	if (r == true) {
 	    var num = parseInt(x);
 		db.transaction(function (tx) {
-			tx.executeSql('INSERT INTO pago_prestamo (prestamo,cuota,key) VALUES ("'+prestamo+'","'+num+'","'+key+'")');
+			tx.executeSql('INSERT INTO pago_prestamo (prestamo,cuota,key,user) VALUES ("'+prestamo+'","'+num+'","'+key+'","'+user+'")');
 			$('#'+prestamo).css({"background-color":"green","color":"#fff"});
 			$('#btn'+prestamo).hide();
 		});
@@ -142,23 +160,21 @@ function upload(){
 			          if(len==0){
 			          	alert('No tienes pagos que subir');
 			          }else{
-
-
-
-					          	  for (i = 0; i < len; i++) {
-					          	  	var uri=url+'prestamos/'+results.rows.item(i).prestamo+'/'+results.rows.item(i).cuota+'/'+results.rows.item(i).key;
+							for (i = 0; i < len; i++) {
+					          	var uri=url+'prestamos/'+results.rows.item(i).prestamo+'/'+results.rows.item(i).cuota+'/'+results.rows.item(i).key+'/'+results.rows.item(i).user;
+								//console.log(uri);
+								var y=0;
+								$.getJSON(uri, function(json, textStatus) {
 									console.log(uri);
 
-									$.getJSON(uri, function(json, textStatus) {
-										console.log(json);
-									});
+								});
 									
-									//console.log(results.rows.item(i));
-
-						          	if(len==i+1){
-					      				db.transaction(function (tx) {tx.executeSql('DELETE FROM pago_prestamo', [], function (tx, results){});});
-						          	}
-						          }
+								if(i+1==len){
+									db.transaction(function (tx) {tx.executeSql('DELETE FROM pago_prestamo', [], function (tx, results){});});
+									alert('Pagos subidos con exito');
+					      			location.href ="app.html";
+								}
+						    }
 			          }
 
 			        });
@@ -167,7 +183,7 @@ function upload(){
 				}
 		}).fail( function() {
 
-		    alert( 'Error intenete mas tarde!!' );
+		    alert( 'Error conexion intenete mas tarde!!' );
 
 		});
 
@@ -180,8 +196,8 @@ function upload(){
 	}
 
 }
-
-	checkConnection();
+checkConnection();
+setInterval(function(){ checkConnection(); }, 3000);
     
     function checkConnection() {
 		$.ajax({
@@ -189,9 +205,10 @@ function upload(){
 		    url: url+'test'
 
 		}).done( function() {
+			console.log('conectado');
 			$('#con').css({"background-color":"green"});
 		}).fail( function() {
-
+			console.log('desconectado');
 		    $('#con').css({"background-color":"red"});
 
 		});
